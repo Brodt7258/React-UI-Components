debed31c60e7bb82f57f41304f1951ce8e5ff3b2
Tim Dill <brodt7258@gmail.com>
2019-05-08 22:08:56 -0400

    begin setting up social card components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <h2>
+      Lambda School
+      <span>@LambdaSchool • 26 jan</span>
+    </h2>
+  );
+  
+};
+
+export default HeaderTitle;
