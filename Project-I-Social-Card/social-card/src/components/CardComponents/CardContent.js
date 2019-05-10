6726260dfa279c75eb8695145a336b2fad83fa8d
Tim Dill <brodt7258@gmail.com>
2019-05-09 20:59:46 -0400

    styled card

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const CardContent = () => {
   return (
-    <div>
+    <div className="card-content">
       <h3>Get started with React</h3>
       <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
       <p>reactjs.org</p>
