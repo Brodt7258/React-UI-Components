c6a69023d286b5e9a66566174d3ba704a76b0b7b
Tim Dill <brodt7258@gmail.com>
2019-05-11 02:23:06 -0400

    refactored App logic into helpers

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,98 +1,16 @@
 import React, { useReducer } from 'react';
 import './App.css';
 
-import { ENTER_NUM, ENTER_OPERATOR, CLEAR, CALCULATE, ADD, SUBTRACT, MULTIPLY, DIVIDE } from './utils/types';
-import { appendDigit } from './utils/helpers';
+import { ENTER_NUM, ENTER_OPERATOR, CLEAR, CALCULATE } from './utils/types';
+import { reducer, initialState } from './utils/helpers';
 
 import Display from './components/DisplayComponents/CalculatorDisplay';
 import NumBtn from './components/ButtonComponents/NumberButton';
 import Keypad from './components/ButtonComponents/Keypad';
 import Operators from './components/ButtonComponents/Operators';
 
-const initialState = {
-  operand1: null,
-  operand2: null,
-  operator: null
-};
-
-const reducer = (state, { type, payload }) => {
-  switch(type) {
-    case ENTER_NUM:
-      return state.operator
-      ? {
-        ...state,
-        operand2: appendDigit(state.operand2, payload)
-      }
-      : {
-        ...state,
-        operand1: appendDigit(state.operand1, payload)
-      };
-    case ENTER_OPERATOR:
-      return {
-        ...state,
-        operator: payload
-      };
-    case CLEAR:
-      return {
-        ...initialState
-      };
-    case CALCULATE:
-      return {
-        operand1: calcResult(state),
-        operand2: null,
-        operator: null
-      };
-    default:
-      return {
-        ...state
-      };
-  }
-};
-
-const calcResult = ({ operand1, operand2, operator }) => {
-  switch (operator) {
-    case ADD:
-      return operand1 + operand2;
-    case SUBTRACT:
-      return operand1 - operand2;
-    case MULTIPLY:
-      return operand1 * operand2;
-    case DIVIDE:
-      return operand1 / operand2;
-    default:
-      return NaN;
-  }
-};
-
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
-
-  const clearState = () => {
-    console.log('clearState');
-    dispatch({
-      type: CLEAR
-    });
-  };
-
-  const enterNum = n => () => {
-    dispatch({
-      type: ENTER_NUM,
-      payload: n
-    });
-  };
-
-  const enterOperator = op => () => {
-    dispatch({
-      type: ENTER_OPERATOR,
-      payload: op
-    });
-  };
-
-  const calculate = () => {
-    dispatch({
-      type: CALCULATE
-    });
-  };
 
   console.log(state);
   return (
@@ -101,10 +19,33 @@ const App = () => {
       <NumBtn
         text="clear"
         gridArea="clear"
-        handleClick={clearState}
+        handleClick={() => {
+          dispatch({
+            type: CLEAR
+          });
+        }}
       />
-      <Keypad handleEnterNum={enterNum} />
-      <Operators handleEnterOp={enterOperator} handleCalc={calculate} />
+      <Keypad 
+        handleEnterNum={n => () => {
+          dispatch({
+            type: ENTER_NUM,
+            payload: n
+          });
+        }}
+      />
+      <Operators
+        handleEnterOp={op => () => {
+          dispatch({
+            type: ENTER_OPERATOR,
+            payload: op
+          });
+        }}
+        handleCalc={() => {
+          dispatch({
+            type: CALCULATE
+          })
+        }}
+      />
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/utils/helpers.js
+++ Project-II-Calculator/calculator/src/utils/helpers.js
@@ -1,3 +1,60 @@
-export const appendDigit = (acc, val) => {
+import { ENTER_NUM, ENTER_OPERATOR, CLEAR, CALCULATE, ADD, SUBTRACT, MULTIPLY, DIVIDE } from './types';
+
+export const initialState = {
+  operand1: null,
+  operand2: null,
+  operator: null
+};
+
+export const reducer = (state, { type, payload }) => {
+  switch(type) {
+    case ENTER_NUM:
+      return state.operator
+      ? {
+        ...state,
+        operand2: appendDigit(state.operand2, payload)
+      }
+      : {
+        ...state,
+        operand1: appendDigit(state.operand1, payload)
+      };
+    case ENTER_OPERATOR:
+      return {
+        ...state,
+        operator: payload
+      };
+    case CLEAR:
+      return {
+        ...initialState
+      };
+    case CALCULATE:
+      return {
+        operand1: calcResult(state),
+        operand2: null,
+        operator: null
+      };
+    default:
+      return {
+        ...state
+      };
+  }
+};
+
+const calcResult = ({ operand1, operand2, operator }) => {
+  switch (operator) {
+    case ADD:
+      return operand1 + operand2;
+    case SUBTRACT:
+      return operand1 - operand2;
+    case MULTIPLY:
+      return operand1 * operand2;
+    case DIVIDE:
+      return operand1 / operand2;
+    default:
+      return NaN;
+  }
+};
+
+const appendDigit = (acc, val) => {
   return (acc * 10) + val;
 };
